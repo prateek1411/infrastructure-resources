6a0a7261ec084b62a243b738f542e8d7333c723f
prateek.srivastava <Prateek1411@gmail.com>
2020-11-27 21:44:42 +0100

    added azurerm as backend

--- infrastructure-resources/main.py
+++ infrastructure-resources/main.py
@@ -1,6 +1,6 @@
 import os
 
-from cdktf import App, TerraformStack
+from cdktf import App, TerraformStack, AzurermBackend
 from constructs import Construct
 from decouple import config
 from Crypto.PublicKey import RSA
@@ -15,17 +15,24 @@ from imports.azurerm import (AzurermProvider, AzurermProviderFeatures,
                              VirtualMachineOsProfileLinuxConfig, VirtualMachineOsProfileLinuxConfigSshKeys,
                              ResourceGroupConfig)
 
+subscription_id = config('subscription_id')
+client_id = config('client_id')
+client_secret = config('client_secret')
+tenant_id = config('tenant_id')
+access_key = config('access_key')
+
 
 class MyVirtualMachine(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
         # define resources here
-        subscription_id = config('subscription_id')
-        client_id = config('client_id')
-        client_secret = config('client_secret')
-        tenant_id = config('tenant_id')
+
+        backend = AzurermBackend(self, resource_group_name='prateek-vm_group', storage_account_name='digirisestatic',
+                                 container_name='tfstate', key="prod.terraform.tfstate.prateek-vm2",
+                                 access_key=access_key)
         features = AzurermProviderFeatures()
+
         provider = AzurermProvider(self, 'azure', features=[features], subscription_id=subscription_id,
                                    client_id=client_id, client_secret=client_secret,
                                    tenant_id=tenant_id)
@@ -58,15 +65,17 @@ class MyVirtualMachine(TerraformStack):
                                   depends_on=[resource_group])
         storage_disk = VirtualMachineStorageOsDisk(name='azure_os_disk', create_option='FromImage', disk_size_gb=50)
         storage_image_ref = VirtualMachineStorageImageReference(offer='UbuntuServer', publisher='Canonical',
-                                                                sku='18.04-LTS',version='latest')
-        os_profile = VirtualMachineOsProfile(admin_username='prateek',computer_name='prateek-vm2')
+                                                                sku='18.04-LTS', version='latest')
+        os_profile = VirtualMachineOsProfile(admin_username='prateek', computer_name='prateek-vm2')
         key = RSA.generate(4086, os.urandom)
         with open(os.path.join("Prateek-vm2.pem"), 'wb') as content_file:
             content_file.write(key.exportKey('PEM'))
             key_data = str(key.publickey().exportKey('OpenSSH').decode("utf-8"))
             print(key_data)
-        ssh_keys = VirtualMachineOsProfileLinuxConfigSshKeys(path='/home/{0}/.ssh/authorized_keys'.format(os_profile.admin_username),key_data=key_data)
-        os_profile_linux_config = VirtualMachineOsProfileLinuxConfig(disable_password_authentication=True, ssh_keys=[ssh_keys])
+        ssh_keys = VirtualMachineOsProfileLinuxConfigSshKeys(
+            path='/home/{0}/.ssh/authorized_keys'.format(os_profile.admin_username), key_data=key_data)
+        os_profile_linux_config = VirtualMachineOsProfileLinuxConfig(disable_password_authentication=True,
+                                                                     ssh_keys=[ssh_keys])
         azure_vm = VirtualMachine(self, 'azure_vm',
                                   location='East US',
                                   name='Prateek-vm2',
@@ -74,7 +83,7 @@ class MyVirtualMachine(TerraformStack):
                                   resource_group_name=resource_group.name,
                                   storage_os_disk=[storage_disk],
                                   storage_image_reference=[storage_image_ref],
-                                  os_profile=[os_profile],#
+                                  os_profile=[os_profile],  #
                                   os_profile_linux_config=[os_profile_linux_config],
                                   vm_size='Standard_D2_v2')
         # cluster = KubernetesCluster(
@@ -90,15 +99,14 @@ class MyVirtualMachine(TerraformStack):
         # )
         #
 
+
 class MyK8S(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
-        # define resources here
-        subscription_id = config('subscription_id')
-        client_id = config('client_id')
-        client_secret = config('client_secret')
-        tenant_id = config('tenant_id')
+        backend = AzurermBackend(self, resource_group_name='prateek-vm_group', storage_account_name='digirisestatic',
+                                 container_name='tfstate', key="prod.terraform.tfstate.k8s",
+                                 access_key=access_key)
         features = AzurermProviderFeatures()
         provider = AzurermProvider(self, 'azure', features=[features], subscription_id=subscription_id,
                                    client_id=client_id, client_secret=client_secret,
@@ -112,21 +120,20 @@ class MyK8S(TerraformStack):
         identity = KubernetesClusterIdentity(type='SystemAssigned')
 
         cluster = KubernetesCluster(
-           self, 'our-kube-cluster',
-           name='our-kube-cluster',
-           default_node_pool=[node_pool],
-           dns_prefix='test',
-           location=resource_group.location,
-           resource_group_name=resource_group.name,
-           identity=[identity],
-           tags={"foo": "bar"}
+            self, 'our-kube-cluster',
+            name='our-kube-cluster',
+            default_node_pool=[node_pool],
+            dns_prefix='test',
+            location=resource_group.location,
+            resource_group_name=resource_group.name,
+            identity=[identity],
+            tags={"foo": "bar"}
         )
 
 
-
-app_vm = App(outdir='vm',stack_traces=False)
-app_k8s = App(outdir='k8s',stack_traces=False)
+app_vm = App(outdir='vm', stack_traces=False)
+app_k8s = App(outdir='k8s', stack_traces=False)
 MyVirtualMachine(app_vm, "virtual-machine")
-MyK8S(app_k8s,"k8s-cluster")
+MyK8S(app_k8s, "k8s-cluster")
 app_k8s.synth()
 app_vm.synth()
